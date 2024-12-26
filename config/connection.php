327923ffc9337e5712f160a56f788ed414a16687
Vrushabh Patel <vrushabhpatel464@gmail.com>
2024-12-26 21:15:32 +0530

    Changes

--- config/connection.php
+++ config/connection.php
@@ -1,18 +1,19 @@
 <?php
 session_start();
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_grocery";
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_grocery";
+} else if ($_SERVER["SERVER_NAME"] == "cloud3.googiehost") {
+    $host = "cloud3.googiehost";
+    $username = "groceryw_develop";
+    $password = "bpP3FKSZQkrz";
+    $dbname = "groceryw_dev"; //hostname()
+}
 
 $db = new PDO("mysql:host=" . $host . ";dbname=" . $dbname . "", $username, $password);
 return $db;
 
-
-// if ($db == true) {
-// 	echo "Connection success";
-// } else {
-// 	echo "Error";
-// }
-// ?>
+?>
