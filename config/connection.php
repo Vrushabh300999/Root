14b21a3b7ecc256465a011aad7a45c7f718a02e6
Vrushabh Patel <vrushabhpatel464@gmail.com>
2025-02-02 19:45:35 +0530

    Changes

--- config/connection.php
+++ config/connection.php
@@ -14,8 +14,11 @@ if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'grocery.whf.bz
 }
 
 $db = new PDO("mysql:host=" . $host . ";dbname=" . $dbname . "", $username, $password);
-return $db;
 
+date_default_timezone_set("Asia/Kolkata");
+define("RECORDS_PER_PAGE", 4);
+define("ROOT", "/");
+include("functions.php");
 
 // if ($db == true) {
 // 	echo "Connection success";
