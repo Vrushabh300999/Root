070694c036251cd50ce01f288cfe323d8d7f3a34
Vrushabh Patel <vrushabhpatel464@gmail.com>
2025-02-02 19:34:21 +0530

    Changes

--- config/connection.php
+++ config/connection.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
-// error_reporting(0);
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_grocery";
+if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'grocery.whf.bz') {
+    $host = "localhost";
+    $username = "groceryw_develop";
+    $password = "bpP3FKSZQkrz";
+    $dbname = "groceryw_dev";
+} else {
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_grocery";
+}
 
 $db = new PDO("mysql:host=" . $host . ";dbname=" . $dbname . "", $username, $password);
+return $db;
 
-date_default_timezone_set("Asia/Kolkata");
-define("RECORDS_PER_PAGE", 4);
-define("ROOT", dirname(__FILE__));
-include("functions.php");
 
 // if ($db == true) {
 // 	echo "Connection success";
